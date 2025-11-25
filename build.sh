aa0ae0d3bf84d99936523c40f3078013ff91f0f9
ikas <65895428+ikas-mc@users.noreply.github.com>
2025-11-25 17:59:28 +0800

    build: fix config

--- build.sh
+++ build.sh
@@ -8,15 +8,13 @@ chmod +x ./project-list.sh
 ./project-list.sh
 
 cd $BASE_PATH
-sed -i 's/src-link ikas/#src-link ikas/g' openwrt-config/openwrt/feeds.conf.default
-echo "src-link ikas ${BASE_PATH}/openwrt-config/ikas-packages" >> openwrt-config/openwrt/feeds.conf.default
 
 # ./scripts/diffconfig.sh > diffconfig
 # cp diffconfig .config
 # wget https://downloads.openwrt.org/releases/24.10.2/targets/ath79/generic/config.buildinfo -O .config
-cp openwrt-config/devices/${DEVICE_NAME}/apk/config.buildinfo openwrt/.config
-cp -r openwrt-config/openwrt/* openwrt/
+cp openwrt-config/devices/${DEVICE_NAME}/apk/.config openwrt/.config
 cp openwrt-config/devices/${DEVICE_NAME}/custom.sh openwrt/custom.sh
+cp -r openwrt-config/openwrt/* openwrt/
 
 cd openwrt
 
@@ -28,6 +26,5 @@ chmod +x ./custom.sh
 
 make defconfig
 
-make -j $(nproc) download
-make -j $(nproc)
-make checksum
+make -j $(nproc) download V=s
+make -j $(nproc) V=s
