9e90221d8075983012569a49eb32f7491ef31837
ikas <65895428+ikas-mc@users.noreply.github.com>
2025-11-26 08:07:13 +0800

    build: add config log

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ cd $BASE_PATH
 # ./scripts/diffconfig.sh > diffconfig
 # cp diffconfig .config
 # wget https://downloads.openwrt.org/releases/24.10.2/targets/ath79/generic/config.buildinfo -O .config
-cp openwrt-config/devices/${DEVICE_NAME}/apk/.config openwrt/.config
+cp openwrt-config/devices/${DEVICE_NAME}/apk/.config openwrt/diffconfig
 cp openwrt-config/devices/${DEVICE_NAME}/custom.sh openwrt/custom.sh
 cp -r openwrt-config/openwrt/* openwrt/
 
@@ -23,10 +23,14 @@ chmod +x ./custom.sh
 
 #https://openwrt.org/docs/guide-developer/toolchain/use-buildsystem
 
+cp diffconfig .config
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+echo "diff .config:"
+cat .config
 
-echo "Current .config:"
+cp diffconfig .config
+echo "build .config:"
 cat .config
 
 make defconfig
