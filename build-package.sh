6afbb663094ca82615ae9f1682a80a578ced1fd0
ikas <65895428+ikas-mc@users.noreply.github.com>
2025-12-04 22:39:14 +0800

    build: add sdk support

---
+++ build-package.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+DEVICE_NAME=$1
+BASE_PATH=$(pwd)
+
+source openwrt-config/devices/${DEVICE_NAME}/config.txt
+
+cd openwrt-config/ikas-packages
+chmod +x ./project-list.sh
+./project-list.sh > /dev/null 2>&1
+cd $BASE_PATH
+
+curl -o openwrt.sdk.tar.zst ${CONFIG_SDK_URL}
+tar --zstd -xvf openwrt.sdk.tar.zst
+mv openwrt-sdk-* openwrt-sdk
+rm openwrt.sdk.tar.zst
+
+cp openwrt-config/devices/${DEVICE_NAME}/apk/sdk_package_config.txt openwrt-sdk/diffconfig
+cp -r openwrt-config/openwrt/* openwrt-sdk/
+
+cd openwrt-sdk
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+cp diffconfig .config
+make defconfig
+
+make -j $(nproc) download world V=sc
